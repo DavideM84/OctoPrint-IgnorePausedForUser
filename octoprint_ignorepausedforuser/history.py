97eab9021d53c66cf6901616e00413ece94e9314
DavideM84 <d.mencarelli@outlook.com>
2021-02-20 10:12:40 +0100

    alignment working dir

---
+++ octoprint_ignorepausedforuser/history.py
@@ -0,0 +1,85 @@
+import os, json, uuid, logging
+from datetime import datetime
+
+class History:
+    
+    def __init__(self, logger, pluginDataFolder, historySize = 10):
+        self.logger = logger
+        self.historyFile = f"{pluginDataFolder}/history.dat"
+        self.historySize = 10 if historySize < 1 or historySize > 1000 else historySize
+        self.job = None
+
+    def StartJob(self, payload):
+        id = str(uuid.uuid4())
+        self.logger.info(f"History > Start job id: '{id}'")
+        self.job = {
+            "id": id,
+            "startedAt": datetime.now().isoformat(),
+            "endedAt": "",
+            "gcode": payload["name"],
+            "size": payload["size"],
+            "origin": payload["origin"],
+            "user": payload["user"],
+            "state": "printing",
+            "pauses": 0
+        }
+        self.addJob()
+
+    def StopJob(self, cancelled):
+        id = self.job["id"]
+        self.logger.info(f"History > Stop job id: '{id}'")
+        self.job["endedAt"] = datetime.now().isoformat()
+        self.job["state"] = "cancelled" if cancelled else "done"
+        self.updateJob()
+        self.job = None
+
+    def UpdateCount(self, count):
+        id = self.job["id"]
+        self.logger.info(f"History > Update job id: '{id}' count: {count}")
+        self.job["pauses"] = count
+        self.updateJob()
+
+    def ClearHistory(self):
+        self.logger.info(f"History > Clear")
+        if os.path.exists(self.historyFile):
+            os.remove(self.historyFile)
+
+    #####
+
+    def addJob(self):
+        if self.job != None:
+            data = self.readHistory()
+            if (len(data["jobs"]) > self.historySize):
+                data["jobs"].clear()
+            data["jobs"].append(self.job)
+            self.writeHistory(data)
+
+    def updateJob(self):
+        if self.job != None:
+            data = self.readHistory()
+            for job in data["jobs"]:
+                if job["id"] == self.job["id"]:
+                    self.copyObject(self.job, job)
+                    self.writeHistory(data)
+                    break
+
+    def readHistory(self):
+        if not os.path.exists(self.historyFile):
+            self.writeHistory({ "jobs": [] })
+        with open(self.historyFile, "r") as f:
+            data = json.load(f)
+        return data
+
+    def writeHistory(self, data):
+        with open(self.historyFile, "w") as f:
+            json.dump(data, f)
+
+    def copyObject(self, src, dest):
+        dest["endedAt"] = src["endedAt"]
+        dest["state"] = src["state"]
+        dest["pauses"] = src["pauses"]
+        #dest["startedAt"] = src["startedAt"]
+        #dest["gcode"] = src["gcode"]
+        #dest["size"] = src["size"]
+        #dest["origin"] = src["origin"]
+        #dest["user"] = src["user"]
---
+++ octoprint_ignorepausedforuser/static/js/IgnorePausedForUser.js
@@ -0,0 +1,52 @@
+$(function() {
+    function IgnorePausedForUserViewModel(parameters) {
+        var self = this;
+		
+		self.settingsViewModel = parameters[0];
+        self.enabled = ko.observable();
+        self.autoclose = ko.observable();
+        self.historySize = ko.observable();
+
+		self.onDataUpdaterPluginMessage = function(plugin, data) {
+            if (plugin !== "ignorepausedforuser") 
+                return;
+
+            if(self.settingsViewModel.settings.plugins.ignorepausedforuser.enabled())
+            {
+                new PNotify({
+                    title: '<p style="text-align:center">Paused for User</p>',
+                    text: '\n' + data.msg,
+                    type: "info",
+                    hide: data.hide
+                    });
+            }
+		}
+		
+		self.onBeforeBinding = function() {
+            self.enabled(self.settingsViewModel.settings.plugins.ignorepausedforuser.enabled());
+            self.autoclose(self.settingsViewModel.settings.plugins.ignorepausedforuser.autoclose());
+            self.historySize(self.settingsViewModel.settings.plugins.ignorepausedforuser.historySize());
+        }
+		
+		self.onEventSettingsUpdated = function (payload) {            
+            self.enabled = self.settingsViewModel.settings.plugins.ignorepausedforuser.enabled();
+            self.autoclose = self.settingsViewModel.settings.plugins.ignorepausedforuser.autoclose();
+            self.historySize = self.settingsViewModel.settings.plugins.ignorepausedforuser.historySize();
+        }
+    }
+
+    // This is how our plugin registers itself with the application, by adding some configuration
+    // information to the global variable OCTOPRINT_VIEWMODELS
+    ADDITIONAL_VIEWMODELS.push([
+        // This is the constructor to call for instantiating the plugin
+        IgnorePausedForUserViewModel,
+
+        // This is a list of dependencies to inject into the plugin, the order which you request
+        // here is the order in which the dependencies will be injected into your view model upon
+        // instantiation via the parameters argument
+        ["settingsViewModel"],
+
+        // Finally, this is the list of selectors for all elements we want this view model to be bound to.
+        ["#settings_plugin_IgnorePausedForUser_form"]
+    ]);
+});
